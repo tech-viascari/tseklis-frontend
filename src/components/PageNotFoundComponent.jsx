e25484c68a67c9ba8adfee2cf78bdc0c010f31f4
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2025-01-28 09:48:22 +0800

    Added: Logo for Tseklis

--- src/components/PageNotFoundComponent.jsx
+++ src/components/PageNotFoundComponent.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { underConstructionSVG } from "./GetIcons";
 import { Typography } from "@material-tailwind/react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import ButtonComponent from "./ButtonComponent";
 import { BiHome } from "react-icons/bi";
 import { HiArrowTurnDownLeft, HiHome } from "react-icons/hi2";
 
 const PageNotFoundComponent = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
@@ -24,12 +25,14 @@ const PageNotFoundComponent = () => {
             <ButtonComponent
               variant="text"
               className="bg-transparent hover:bg-primary"
+              onClick={() => navigate("/")}
             >
               <HiHome size={20} />
             </ButtonComponent>
             <ButtonComponent
               variant="outlined"
               className="bg-transparent hover:bg-primary"
+              onClick={() => navigate(-1)}
             >
               <HiArrowTurnDownLeft />
             </ButtonComponent>
