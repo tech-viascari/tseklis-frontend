1d3bbd591cae8fc98bbffd377b738997d28c8dbe
Anthony-0801 <zyxanthony15@gmail.com>
2025-05-13 11:01:29 +0800

    Add stockholder options for Independent Auditor's Report in AddDocumentDraftingPage

--- src/pages/legal_entities/legal_entity/document_drafting/AddDocumentDraftingPage.jsx
+++ src/pages/legal_entities/legal_entity/document_drafting/AddDocumentDraftingPage.jsx
@@ -91,6 +91,134 @@ const AddDocumentDraftingPage = () => {
     setPageIsLoading(false);
   };
 
+  const noOfStockholders = [
+    {
+      number: "One",
+      iar_no_of_stockholders: "one",
+      iar_no_of_stockholders_int: "1",
+    },
+    {
+      number: "Two",
+      iar_no_of_stockholders: "two",
+      iar_no_of_stockholders_int: "2",
+    },
+    {
+      number: "Three",
+      iar_no_of_stockholders: "three",
+      iar_no_of_stockholders_int: "3",
+    },
+    {
+      number: "Four",
+      iar_no_of_stockholders: "four",
+      iar_no_of_stockholders_int: "4",
+    },
+    {
+      number: "Five",
+      iar_no_of_stockholders: "five",
+      iar_no_of_stockholders_int: "5",
+    },
+    {
+      number: "Six",
+      iar_no_of_stockholders: "six",
+      iar_no_of_stockholders_int: "6",
+    },
+    {
+      number: "Seven",
+      iar_no_of_stockholders: "seven",
+      iar_no_of_stockholders_int: "7",
+    },
+    {
+      number: "Eight",
+      iar_no_of_stockholders: "eight",
+      iar_no_of_stockholders_int: "8",
+    },
+    {
+      number: "Nine",
+      iar_no_of_stockholders: "nine",
+      iar_no_of_stockholders_int: "9",
+    },
+    {
+      number: "Ten",
+      iar_no_of_stockholders: "ten",
+      iar_no_of_stockholders_int: "10",
+    },
+    {
+      number: "Eleven",
+      iar_no_of_stockholders: "eleven",
+      iar_no_of_stockholders_int: "11",
+    },
+    {
+      number: "Twelve",
+      iar_no_of_stockholders: "twelve",
+      iar_no_of_stockholders_int: "12",
+    },
+    {
+      number: "Thirteen",
+      iar_no_of_stockholders: "thirteen",
+      iar_no_of_stockholders_int: "13",
+    },
+    {
+      number: "Fourteen",
+      iar_no_of_stockholders: "fourteen",
+      iar_no_of_stockholders_int: "14",
+    },
+    {
+      number: "Fifteen",
+      iar_no_of_stockholders: "fifteen",
+      iar_no_of_stockholders_int: "15",
+    },
+    {
+      number: "Sixteen",
+      iar_no_of_stockholders: "sixteen",
+      iar_no_of_stockholders_int: "16",
+    },
+    {
+      number: "Seventeen",
+      iar_no_of_stockholders: "seventeen",
+      iar_no_of_stockholders_int: "17",
+    },
+    {
+      number: "Eighteen",
+      iar_no_of_stockholders: "eighteen",
+      iar_no_of_stockholders_int: "18",
+    },
+    {
+      number: "Nineteen",
+      iar_no_of_stockholders: "nineteen",
+      iar_no_of_stockholders_int: "19",
+    },
+    {
+      number: "Twenty",
+      iar_no_of_stockholders: "twenty",
+      iar_no_of_stockholders_int: "20",
+    },
+    {
+      number: "Twenty-One",
+      iar_no_of_stockholders: "twenty-one",
+      iar_no_of_stockholders_int: "21",
+    },
+    {
+      number: "Twenty-Two",
+      iar_no_of_stockholders: "twenty-two",
+      iar_no_of_stockholders_int: "22",
+    },
+    {
+      number: "Twenty-Three",
+      iar_no_of_stockholders: "twenty-three",
+      iar_no_of_stockholders_int: "23",
+    },
+    {
+      number: "Twenty-Four",
+      iar_no_of_stockholders: "twenty-four",
+      iar_no_of_stockholders_int: "24",
+    },
+    {
+      number: "Twenty-Five",
+      iar_no_of_stockholders: "twenty-five",
+      iar_no_of_stockholders_int: "25",
+    },
+  ];
+
   const getFormState = (title, form_contents) => {
     const formState = {
       title: "",
@@ -111,7 +239,7 @@ const AddDocumentDraftingPage = () => {
         formData={formData}
         setFormData={setFormData}
         errors={errors}
-        onChange={() => { }}
+        onChange={() => {}}
       />
     ),
     getFormState(
@@ -120,7 +248,7 @@ const AddDocumentDraftingPage = () => {
         formData={formData}
         setFormData={setFormData}
         errors={errors}
-        onChange={() => { }}
+        onChange={() => {}}
         officers={officers}
         setOfficers={setOfficers}
         selectedOfficer={selectedOfficer}
@@ -132,7 +260,7 @@ const AddDocumentDraftingPage = () => {
         formData={formData}
         setFormData={setFormData}
         errors={errors}
-        onChange={() => { }}
+        onChange={() => {}}
         isPreview={false}
       />
     ),
@@ -156,8 +284,10 @@ const AddDocumentDraftingPage = () => {
       new_form_data.contact_person_email = entity.entity_details.official_email;
 
       //for cover sheet - company info
-      new_form_data.telephone_number = entity.entity_details.official_contact_number;
-      new_form_data.official_mobile_number = entity.entity_details.official_contact_number;
+      new_form_data.telephone_number =
+        entity.entity_details.official_contact_number;
+      new_form_data.official_mobile_number =
+        entity.entity_details.official_contact_number;
 
       if (entity.entity_details.officer_information.length != 0) {
         let officer = entity.entity_details.officer_information[0];
@@ -169,7 +299,6 @@ const AddDocumentDraftingPage = () => {
 
       let officers = entity.entity_details.officer_information.filter(
         (officer) => {
-
           if (officer.officer.includes("secretary").toString().toLowerCase()) {
             new_form_data.corp_sec = officer.officer_name;
             new_form_data.corp_sec_address = officer.current_residence;
@@ -200,14 +329,33 @@ const AddDocumentDraftingPage = () => {
           position: "Stockholder",
         }));
 
-      new_form_data.stockholders = mapped_stockholders_name;
+        new_form_data.stockholders = mapped_stockholders_name;
 
-
-      //for Waiver of Notice - Corporate Secretary
-      let corp_sec_name = entity.latest_GIS[0].document_data.corporate_secretary;
-      new_form_data.nom_corpsec_name = corp_sec_name;
+        //for Waiver of Notice - Corporate Secretary
+        let corp_sec_name =
+          entity.latest_GIS[0].document_data.corporate_secretary;
+        new_form_data.nom_corpsec_name = corp_sec_name;
       } else {
         new_form_data.stockholders = [];
+      }
+
+      //for Independent Auditor's Report - Stockholders that has 100 or more shares
+      if (entity.latest_GIS.length != 0) {
+        let stockholders_amount =
+          entity.latest_GIS[0].document_data.stock_holders_information.information.filter(
+            (stockholder) => stockholder.amount >= 100
+          ).length;
+      
+        if (stockholders_amount) {
+          const matchedStockholder = noOfStockholders.find(
+            (stockholder) => stockholder.iar_no_of_stockholders_int === stockholders_amount.toString()
+          );
+      
+          if (matchedStockholder) {
+            new_form_data.iar_no_of_stockholders = matchedStockholder.iar_no_of_stockholders;
+            new_form_data.iar_no_of_stockholders_int = matchedStockholder.iar_no_of_stockholders_int;
+          }
+        }
       }
 
       setOfficers(officers);
